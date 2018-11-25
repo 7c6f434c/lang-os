78cf465fb0359478fa8a9d68aff2c82722e28f0d
Michael Raskin <7c6f434c@mail.ru>
2018-11-26 02:45:54 +0300

    Syncing from Monotone at 20181126-024554. Last commit message was Add adb to path

--- local/test-system.nix
+++ local/test-system.nix
@@ -127,6 +127,7 @@ pkgs.lib.makeExtensible (self: with self; {
         nsjail unionfs-fuse
         lispPackages.stumpwm alsaTools
         xdummy pv mercurial fossil lvm2 rsync gawk
+        androidenv.platformTools
         (swPieces.cProgram "vtlock" ../c/vtlock.c [] [])
         (swPieces.cProgram "file-lock" ../c/file-lock.c [] [])
         (swPieces.cProgram "in-pty" ../c/in-pty.c [] ["-lutil"])
