48533a2dd1ee93215156293d9202e00c40c65982
Michael Raskin <7c6f434c@mail.ru>
2019-04-23 02:46:26 +0200

    Syncing from Monotone at 20190423-024625. Last commit message was History dump grows too quiclky

--- rlwrap-user-shell.sh
+++ rlwrap-user-shell.sh
@@ -4,7 +4,6 @@ history="$(mktemp ~/.user-lisp-shell_history.XXXXXXXX)"
 cat ~/.user-lisp-shell_history > "$history"
 rlwrap -C user-lisp-shell -D 2 -s 1000000  -H "$history" -f . -c -e '' -b "'\"() " -q "\"" "$@"
 cat "$history" >> ~/.user-lisp-shell_history
-cat "$history" >> ~/.user-lisp-shell_history_dump
 cat ~/.user-lisp-shell_history | sort | uniq > ~/.user-lisp-shell_history.new
 mv  ~/.user-lisp-shell_history.new  ~/.user-lisp-shell_history
 rm "$history"
