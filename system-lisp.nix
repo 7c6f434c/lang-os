6e46122e4ae4b9bc0b889f6be7129d7e9be0909c
Michael Raskin <7c6f434c@mail.ru>
2019-06-12 02:47:23 +0200

    Syncing from Monotone at 20190612-024723. Last commit message was more stuff

--- system-lisp.nix
+++ system-lisp.nix
@@ -11,6 +11,8 @@ pkgs.writeScript "system-lisp-launcher" ''
         (s: ''source "${s}/lib/common-lisp-settings"/*-path-config.sh;'')
         deps
       }
+      export LANG=en_US.UTF-8
+      export LOCALE_ARCHIVE=/var/current-system/sw/lib/locale/locale-archive
       NIX_LISP_ASDF_LOAD='(mapcar (function require) (list :asdf :sb-posix :sb-bsd-sockets))' NIX_LISP_PRELAUNCH_HOOK="nix_lisp_run_single_form '(load \"${pkgs.writeText "system-lisp-script.lisp" code}\")'" NIX_LISP_COMMAND="${pkgs.sbcl}/bin/sbcl" NIX_LISP_EARLY_OPTIONS="$NIX_LISP_EARLY_OPTIONS --noinform --dynamic-space-size 2048" ${pkgs.lispPackages.clwrapper}/bin/common-lisp.sh < /dev/null &>/dev/${tty}
     )
 ''
