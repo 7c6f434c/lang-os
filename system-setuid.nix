fa6cb934b590d8d6d97c79acab6c76ce10ed00aa
Michael Raskin <7c6f434c@mail.ru>
2023-09-18 02:56:11 +0200

    Syncing from Monotone at 20230918-025611. Last commit message was Fixups for rebuild

--- system-setuid.nix
+++ system-setuid.nix
@@ -1,25 +1,30 @@
 {
   pkgs ? import <nixpkgs> {}, setuidPrograms
 }:
+let proglist = (if builtins.isFunction setuidPrograms then (setuidPrograms pkgs) else setuidPrograms); in
 pkgs.runCommand "setuid-set" {} ''
     mkdir "$out"
     wrapper_dir="/var/setuid-wrapper-storage/$(basename "$out")"
 
-    ${pkgs.gcc}/bin/gcc -Wall -O2 -DWRAPPER_DIR="\"$wrapper_dir\"" ${<nixpkgs> + "/nixos/modules/security/wrappers/wrapper.c"} -o "$out/canonical-wrapper" -L${pkgs.libcap.lib}/lib -I${pkgs.libcap.dev}/include -L${pkgs.libcap_ng}/lib -I${pkgs.libcap_ng}/include -lcap -lcap-ng
+    ${ pkgs.lib.concatMapStrings (x:
+    ''
+    ${pkgs.gcc}/bin/gcc -Wall -O2 -DWRAPPER_DIR="\"$wrapper_dir\"" -DSOURCE_PROG="\"${x.src}\"" ${<nixpkgs> + "/nixos/modules/security/wrappers/wrapper.c"} -o "$out/${x.name}" -L${pkgs.libcap.lib}/lib -I${pkgs.libcap.dev}/include -L${pkgs.libcap_ng}/lib -I${pkgs.libcap_ng}/include -lcap -lcap-ng
+    ''
+    ) proglist}
 
     echo "#!${pkgs.stdenv.shell}
       ${
         pkgs.lib.concatMapStrings (x:
           ''
             mkdir -p \"$wrapper_dir\"
-            cp \"$out/canonical-wrapper\" \"$wrapper_dir/${x.name}\"
+            cp \"$out/${x.name}\" \"$wrapper_dir/${x.name}\"
             echo -n \"${x.src}\" >  \"$wrapper_dir/${x.name}.real\"
             chown ${x.owner or "0"}:${x.group or "0"} \"$wrapper_dir/${x.name}\"
             ${pkgs.lib.optionalString x.setuid or false ''chmod u+s \"$wrapper_dir/${x.name}\"''}
             ${pkgs.lib.optionalString x.setgid or false ''chmod g+s \"$wrapper_dir/${x.name}\"''}
             ${pkgs.lib.optionalString (x.setcap or null != null) ''${pkgs.libcap}/bin/setcap \"${x.setcap}\" \"$wrapper_dir/${x.name}\"''}
           ''
-        ) (if builtins.isFunction setuidPrograms then (setuidPrograms pkgs) else setuidPrograms)
+        ) proglist
       }
     " > "$out/install-script"
     chmod a+x "$out/install-script"
